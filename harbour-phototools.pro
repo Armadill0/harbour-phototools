cee476a590a672dbd73b346014144f8955d2cc3b
Thomas Amler <thomas.amler@1und1.de>
2015-01-08 16:14:18 +0100

    initial commit, nothing spectacular

---
+++ harbour-phototools.pro
@@ -0,0 +1,32 @@
+# NOTICE:
+#
+# Application name defined in TARGET has a corresponding QML filename.
+# If name defined in TARGET is changed, the following needs to be done
+# to match new name:
+#   - corresponding QML filename must be changed
+#   - desktop icon filename must be changed
+#   - desktop filename must be changed
+#   - icon definition filename in desktop file must be changed
+#   - translation filenames have to be changed
+
+# The name of your application
+TARGET = harbour-phototools
+
+CONFIG += sailfishapp
+
+SOURCES += src/harbour-phototools.cpp
+
+OTHER_FILES += qml/harbour-phototools.qml \
+    qml/pages/CoverPage.qml \
+    rpm/harbour-phototools.changes.in \
+    rpm/harbour-phototools.spec \
+    rpm/harbour-phototools.yaml \
+    translations/*.ts \
+    harbour-phototools.desktop \
+    qml/pages/DepthOfField.qml
+
+# to disable building translations every time, comment out the
+# following CONFIG line
+CONFIG += sailfishapp_i18n
+TRANSLATIONS += translations/*.ts
+
---
+++ translations/*.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="38"/>
+        <source>My Cover</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DepthOfField</name>
+    <message>
+        <location filename="../qml/pages/DepthOfField.qml" line="45"/>
+        <source>Show Page 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DepthOfField.qml" line="61"/>
+        <source>Depth of field</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DepthOfField.qml" line="67"/>
+        <source>Aperture</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DepthOfField.qml" line="79"/>
+        <source>Sensor format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DepthOfField.qml" line="92"/>
+        <source>Sensor resolution (megapixels)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DepthOfField.qml" line="103"/>
+        <source>Crop factor</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
